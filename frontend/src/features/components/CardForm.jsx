5bb696abcb515bdf59f6af8e6e3277203c4bbaa6
kindluv <onyeukwuamara@gmail.com>
2023-09-23 14:20:49 +0100

    [size: xs]/[feat: made the button disabled when inputs are not filled]

--- frontend/src/features/components/CardForm.jsx
+++ frontend/src/features/components/CardForm.jsx
@@ -3,8 +3,8 @@ import Styles from '../styles/CardForm.module.css';
 import CheckerModal from '../reusables/CheckerModal';
 import axios from 'axios';
 import {validateUrl} from '../../api/Api';
-import GreenChecker from '../../assets/Checker.svg'
-import RedChecker from '../../assets/RedChecker.svg'
+import GreenChecker from '../../assets/Checker.svg';
+import RedChecker from '../../assets/RedChecker.svg';
 import Card from './Card';
 
 const CardForm = () => {
@@ -18,42 +18,53 @@ const CardForm = () => {
 
   const [isOpen, setIsOpen] = useState (false);
   const [isRed, setIsRed] = useState (false);
-  const[isAmericaExpressCard, setIsAmericaExpressCard] = useState(false);
-  const [colorChange, setColorChange] = useState(null);
+  const [isAmericaExpressCard, setIsAmericaExpressCard] = useState (false);
+  const [colorChange, setColorChange] = useState (null);
+  const [isFormValid, setIsFormValid] = useState (false);
 
   const inputRefs = {
-    expiresMonth: useRef(null),
-    expiresYear: useRef(null),
-    cvv: useRef(null),
+    expiresMonth: useRef (null),
+    expiresYear: useRef (null),
+    cvv: useRef (null),
   };
-  
-  const [isLoading, setIsLoading] = useState(false);
+
+  const [isLoading, setIsLoading] = useState (false);
 
   const handleInputChange = (e, inputName) => {
-    const { name, value } = e.target;
-    setFormData({
+    const {name, value} = e.target;
+    setFormData ({
       ...formData,
       [name]: value,
     });
 
-    if (inputName !== 'cvv' && inputName === 'expiresMonth' && value.length === 2) {
-      const nextInputName = getNextInputName(inputName);
+    checkFormValidity ();
+
+    if (
+      inputName !== 'cvv' &&
+      inputName === 'expiresMonth' &&
+      value.length === 2
+    ) {
+      const nextInputName = getNextInputName (inputName);
       if (nextInputName) {
-        inputRefs[nextInputName].current.focus();
+        inputRefs[nextInputName].current.focus ();
       }
     }
 
-    if (inputName !== 'cvv' && inputName === 'expiresYear' && value.length === 2) {
-      const nextInputName = getNextInputName(inputName);
+    if (
+      inputName !== 'cvv' &&
+      inputName === 'expiresYear' &&
+      value.length === 2
+    ) {
+      const nextInputName = getNextInputName (inputName);
       if (nextInputName) {
-        inputRefs[nextInputName].current.focus();
+        inputRefs[nextInputName].current.focus ();
       }
     }
   };
 
-  const getNextInputName = (currentInputName) => {
-    const inputNames = Object.keys(inputRefs);
-    const currentIndex = inputNames.indexOf(currentInputName);
+  const getNextInputName = currentInputName => {
+    const inputNames = Object.keys (inputRefs);
+    const currentIndex = inputNames.indexOf (currentInputName);
 
     if (currentIndex === -1 || currentIndex === inputNames.length - 1) {
       return null;
@@ -63,132 +74,181 @@ const CardForm = () => {
 
   const handleSubmit = async e => {
     e.preventDefault ();
-    setIsLoading(true);
+    setIsLoading (true);
     try {
-      const formattedDate = `${formData.expiresMonth}/${formData.expiresYear.slice(-2)}`;
+      const formattedDate = `${formData.expiresMonth}/${formData.expiresYear.slice (-2)}`;
       const data = {
         cardNumber: formData.cardNumber,
         cardCVV: formData.cvv,
         cardExpiryDate: formattedDate,
       };
-      const response = await axios.post(validateUrl, data);
-      if(response.status === 200){
-        setIsOpen(!isOpen);
-        setColorChange('green');
+      const response = await axios.post (validateUrl, data);
+      if (response.status === 200) {
+        setIsOpen (!isOpen);
+        setColorChange ('green');
       }
     } catch (error) {
-      console.error('Error:', error);
-      setColorChange('red');
-      setIsRed(!isRed)
+      console.error ('Error:', error);
+      setColorChange ('red');
+      setIsRed (!isRed);
     } finally {
-      setIsLoading(false);
+      setIsLoading (false);
     }
   };
 
-  const isAmericanExpress = useMemo(() => {
-    const cardPrefix = formData.cardNumber.slice(0, 2);
-    return cardPrefix === '37' || cardPrefix === '34';
-  }, [formData.cardNumber]);
-  
+  const isAmericanExpress = useMemo (
+    () => {
+      const cardPrefix = formData.cardNumber.slice (0, 2);
+      return cardPrefix === '37' || cardPrefix === '34';
+    },
+    [formData.cardNumber]
+  );
 
-  useMemo(() => {
-    setIsAmericaExpressCard(isAmericanExpress);
-  }, [isAmericanExpress]);
+  useMemo (
+    () => {
+      setIsAmericaExpressCard (isAmericanExpress);
+    },
+    [isAmericanExpress]
+  );
+
+  const checkFormValidity = () => {
+    const {cardNumber, expiresMonth, expiresYear, fullName, cvv} = formData;
+
+    const isValid =
+      cardNumber.trim () !== '' &&
+      expiresMonth.trim () !== '' &&
+      expiresYear.trim () !== '' &&
+      fullName.trim () !== '' &&
+      cvv.trim () !== '';
+
+    setIsFormValid (isValid);
+  };
 
   return (
     <div className={`${Styles.container} ${isOpen && Styles.centerContainer}`}>
-      <Card formData={formData} isAmericaExpressCard={isAmericaExpressCard} colorChange={colorChange}   />
-      {!isOpen &&<form className={`${Styles.form} ${isOpen && Styles.disableForm}`} onSubmit={handleSubmit}>
-        <div className={Styles.fullNameContain}>
-          <p>Card Holder</p>
-          <input
-            type="text"
-            id={Styles.fullName}
-            name="fullName"
-            value={formData.fullName}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className={Styles.inputField}>
-          <p>Card Number</p>
-          <div className={Styles.inputFields}>
-            <div className={Styles.cardNumberOne}>
-              <input
-                maxLength={19}
-                minLength={16}
-                type="text"
-                id={Styles.cardNumber}
-                name="cardNumber"
-                value={formData.cardNumber}
-                onChange={(e)=>handleInputChange(e, 'cardNumber')}
-                placeholder="0000 0000 0000 0000"
-              />
+      <Card
+        formData={formData}
+        isAmericaExpressCard={isAmericaExpressCard}
+        colorChange={colorChange}
+      />
+      {!isOpen &&
+        <form
+          className={`${Styles.form} ${isOpen && Styles.disableForm}`}
+          onSubmit={handleSubmit}
+        >
+          <div className={Styles.fullNameContain}>
+            <p>Card Holder</p>
+            <input
+              required
+              type="text"
+              id={Styles.fullName}
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={Styles.inputField}>
+            <p>Card Number</p>
+            <div className={Styles.inputFields}>
+              <div className={Styles.cardNumberOne}>
+                <input
+                  required
+                  maxLength={19}
+                  minLength={16}
+                  type="text"
+                  id={Styles.cardNumber}
+                  name="cardNumber"
+                  value={formData.cardNumber}
+                  onChange={e => handleInputChange (e, 'cardNumber')}
+                  placeholder="0000 0000 0000 0000"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={Styles.rightDiv}>
-          <div className={Styles.expiryDate}>
-            <p>Exp. Date</p>
-            <div className={Styles.expireForm}>
-              <input
-                maxLength={2}
-                type="text"
-                id={Styles.expiresMonth}
-                name="expiresMonth"
-                value={formData.expiresMonth}
-                onChange={(e)=>handleInputChange(e, 'expiresMonth')}
-                placeholder="12"
-                ref={inputRefs.expiresMonth}
-              />
-              <span>/</span>
-              <input
-                maxLength={2}
-                type="text"
-                id={Styles.expiresYear}
-                name="expiresYear"
-                value={formData.expiresYear}
-                onChange={(e)=>handleInputChange(e, 'expiresYear')}
-                placeholder="23"
-                ref={inputRefs.expiresYear}
-              />
+          <div className={Styles.rightDiv}>
+            <div className={Styles.expiryDate}>
+              <p>Exp. Date</p>
+              <div className={Styles.expireForm}>
+                <input
+                  required
+                  maxLength={2}
+                  type="text"
+                  id={Styles.expiresMonth}
+                  name="expiresMonth"
+                  value={formData.expiresMonth}
+                  onChange={e => handleInputChange (e, 'expiresMonth')}
+                  placeholder="12"
+                  ref={inputRefs.expiresMonth}
+                />
+                <span>/</span>
+                <input
+                  required
+                  maxLength={2}
+                  type="text"
+                  id={Styles.expiresYear}
+                  name="expiresYear"
+                  value={formData.expiresYear}
+                  onChange={e => handleInputChange (e, 'expiresYear')}
+                  placeholder="23"
+                  ref={inputRefs.expiresYear}
+                />
+              </div>
             </div>
+            {isAmericaExpressCard
+              ? <div className={Styles.formGroupCvv}>
+                  <p>CVV</p>
+                  <input
+                    required
+                    maxLength={4}
+                    type="text"
+                    id={Styles.cvv}
+                    name="cvv"
+                    value={formData.cvv}
+                    placeholder="CVV"
+                    onChange={e => handleInputChange (e, 'cvv')}
+                    ref={inputRefs.cvv}
+                  />
+                </div>
+              : <div className={Styles.formGroupCvv}>
+                  <p>CVV</p>
+                  <input
+                    required
+                    maxLength={3}
+                    type="text"
+                    id={Styles.cvv}
+                    name="cvv"
+                    value={formData.cvv}
+                    placeholder="CVV"
+                    onChange={e => handleInputChange (e, 'cvv')}
+                    ref={inputRefs.cvv}
+                  />
+                </div>}
           </div>
-          {isAmericaExpressCard ? (
-            <div className={Styles.formGroupCvv}>
-              <p>CVV</p>
-              <input
-                maxLength={4}
-                type="text"
-                id={Styles.cvv}
-                name="cvv"
-                value={formData.cvv}
-                placeholder="CVV"
-                onChange={(e) => handleInputChange(e, 'cvv')}
-                ref={inputRefs.cvv}
-              />
-            </div>
-          ) : (
-            <div className={Styles.formGroupCvv}>
-              <p>CVV</p>
-              <input
-                maxLength={3}
-                type="text"
-                id={Styles.cvv}
-                name="cvv"
-                value={formData.cvv}
-                placeholder="CVV"
-                onChange={(e) => handleInputChange(e, 'cvv')}
-                ref={inputRefs.cvv}
-              />
-            </div>
-          )}
-        </div>
-        <div className={Styles.btn}>
-          <button type="submit">{isLoading ? 'Loading...' : 'Submit'}</button>
-        </div>
-      </form>}
-      {isRed && <CheckerModal img={RedChecker} text={'Invalid Credit Card'} btnText={'Try Again'}  />}
-      {isOpen && <CheckerModal img={GreenChecker} text={'Valid Credit Card'} btnText={'close Modal'} />}
+          <div className={Styles.btn}>
+            <button
+              type="submit"
+              disabled={!isFormValid}
+              style={{
+                backgroundColor: isFormValid ? '' : 'gray',
+                color: isFormValid ? '' : 'white',
+              }}
+            >
+              {isLoading ? 'Loading...' : 'Submit'}
+            </button>
+          </div>
+        </form>}
+      {isRed &&
+        <CheckerModal
+          img={RedChecker}
+          text={'Invalid Credit Card'}
+          btnText={'Try Again'}
+        />}
+      {isOpen &&
+        <CheckerModal
+          img={GreenChecker}
+          text={'Valid Credit Card'}
+          btnText={'close Modal'}
+        />}
     </div>
   );
 };
